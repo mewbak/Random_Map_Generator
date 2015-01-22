a52c0adb83027377f0fea107903a2cfeb29a0b97
Paul-Wortmann <paul.wortmann@gmail.com>
2015-01-23 00:33:13 +0900

    Added base of D1.

---
+++ src/MapGenerator_D1.cpp
@@ -0,0 +1,30 @@
+/*
+    Random Map Generator.
+    Copyright (C) 2014  Paul Wortmann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    paul.wortmann@gmail.com
+*/
+
+#include "MapGenerator.h"
+#include "MapGenerator_D1.h"
+
+void MapGenerator_D1::Generate (MapType* map_pointer)
+{
+    MapGenerator_D1::Initialize(map_pointer);
+    // generate in map_pointer
+    MapGenerator_D1::Export(map_pointer);
+}
+
---
+++ src/MapGenerator_D1.h
@@ -0,0 +1,32 @@
+/*
+    Random Map Generator.
+    Copyright (C) 2014  Paul Wortmann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    paul.wortmann@gmail.com
+*/
+
+#ifndef MAP_GENERATOR_D1_H
+#define MAP_GENERATOR_D1_H
+
+#include "MapGenerator.h"
+
+class MapGenerator_D1 : public MapGenerator
+{
+    public:
+        void Generate (MapType* map_pointer);
+};
+
+#endif // MAP_GENERATOR_D1_H
